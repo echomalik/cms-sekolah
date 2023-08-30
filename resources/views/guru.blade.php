47a0735b9cb896878c1a5159fbfbb25b50052435
Zefery <rzefery@gmail.com>
2023-08-30 08:55:33 +0700

    test

--- resources/views/guru.blade.php
+++ resources/views/guru.blade.php
@@ -5,15 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+
     <link rel="stylesheet" href="{{ asset('css/guru.css') }}">
 </head>
 <body>
   {{-- navbar --}}
-  <nav class="navbar shadow p-3 mb-6">
+  <nav class="navbar shadow p-2 mb-6">
     <div class="container">
       <a class="navbar-brand" href="#">
-        <img src="{{url('views/smktelkomjkt1.png')}}" alt="SMK Telkom" width="30" height="24">
+        <img src="{{ asset('img/smktelkomjkt 1.png') }}" alt="SMK Telkom" width="120" height="55">
       </a>
       <div class="d-flex flex-row-reverse">
         <div class="p-1"><a class="btn" href="">Contact</a></div>
@@ -26,9 +30,10 @@
           </a>
         
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="#">Struktur Osis dan MPK</a></li>
+            <li><a class="dropdown-item" href="#">Prestasi Siswa</a></li>
+            <li><a class="dropdown-item" href="#"> Ekstrakulikuler </a></li>
+            <li><a class="dropdown-item" href="#"> Daftra Alumni </a></li>
           </ul>
         </div>
 
@@ -38,9 +43,8 @@
           </a>
         
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="#">Daftar Wali Kelas</a></li>
+            <li><a class="dropdown-item" href="#">Kejuruan</a></li>
           </ul>
         </div>
 
@@ -51,6 +55,48 @@
     </div>
   </nav>
   {{-- end navbar --}}
+
+  <div class="isi"></div>
+
+  {{-- footer --}}
+  <div class="container footer vh-100">
+    <div class="kiri bg-success">
+
+      <div class="gambar">
+        <img src="{{ asset('img/smktelkomjkt 1.png') }}" alt="SMK Telkom" width="120" height="55">
+      </div>
+
+      <div class="alamat col-6 p-4 my-auto">
+        Alamat : Jl. Daan Mogot KM.11 RT.01/RW.04, Jl. Raya Daan Mogot No.KM. 11, RT.1/RW.4, Kedaung Kali Angke, Kecamatan Cengkareng, Kota Jakarta Barat, Daerah Khusus Ibukota Jakarta 11710
+      </div>
+      <div class="keterangan my-auto col-6 p-4">
+        <p>Sekolah Berintegritas yang bernaung di bawah Yayasan Pendidikan Telkom.</p> 
+        <p>021-5442800 atau WA 08111442600</p>
+      </div>
+    </div>
+
+    <div class="kanan bg-primary col-3 p-4 my-auto">
+      <center>Follow us</center>
+      <div class="social mx-auto my-auto d-flex justify-content-center h-100">
+        <div class="facebook">
+          <a style="text-decoration: none," href="a">
+            <i class="bi bi-facebook p-1 my-auto" style="font-size: 2rem; color:black;"></i>
+          </a>
+        </div>
+        <div class="instagram">
+          <a style="text-decoration: none," href="b">
+            <i class="bi bi-instagram p-5 my-auto" style="font-size: 2rem; color:black;"></i>
+          </a>
+        </div>
+        <div class="twitter">
+          <a style="text-decoration: none," href="s">
+            <i class="bi bi-twitter p-1 my-auto" style="font-size: 2rem; color:black;"></i>
+          </a>        
+        </div>
+      </div>
+    </div>
+  </div>
+  {{-- end footer --}}
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
