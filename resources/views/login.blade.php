17a824f3bdd3b6884a1b65a07f0e63629c486430
pomiz <azdkaprattama22@gmail.com>
2023-08-31 18:27:00 +0700

    login bc

---
+++ resources/views/login.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="{{ asset('css/css.css') }}">
+  </head>
+  <body>
+    <div class="LOGIN-BC">
+      <div class="div">
+        <div class="welcome-in-business">Welcome,<br />In business Center</div>
+        <div class="overlap">
+          <div class="group">
+            <div class="overlap-group"><div class="text-wrapper">Log-in</div></div>
+          </div>
+    <form>
+          <div class="group-2">
+            <div class="rectangle">
+                <input type="text" name="password" class="form_login" placeholder="Password ..">
+            </div>
+            <div class="text-wrapper-2">Password :</div>
+          </div>
+          <div class="group-3">
+            <div class="rectangle"> 
+                <input type="text" name="Email" class="form_login" placeholder="Email ..">
+            </div>
+            <div class="text-wrapper-2" >Email :</div>
+          </div>
+          <div class="group-4">
+            <button class="button-3" role="button">Submit</button>
+            </div>
+          </div>
+    </form> 
+        <div class="navbar-style">
+          <div class="frame">
+            <div class="overlap-group-wrapper">
+              <div class="log-in-wrapper"><div class="log-in">&nbsp;&nbsp;Log-In</div></div>
+            </div>
+          </div>
+          <div class="top-nav">
+            <div class="div-wrapper"><div class="text-wrapper-4">Business Center</div></div>
+          </div>
+          <img class="smktelkomjkt" src="img/smktelkomjkt-4.png" />
+          <div class="li-sec">
+            <div class="a"><div class="link">Home</div></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html> 
